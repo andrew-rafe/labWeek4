d74e0f1b46e63ecff592015fa092be6c51ad583c
Andrew Rafe <98134152_admin@FEIT025501.adsroot.uts.edu.au>
2019-08-12 12:34:41 +1000

    80percent complete

---
+++ Assets/Scripts/ConsolePrint.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConsolePrint : MonoBehaviour
+{
+
+    private int counter = 0;
+    private string name;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = this.gameObject.name;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(name + " : " + counter++);
+    }
+}
